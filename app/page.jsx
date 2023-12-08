ab7052488bcf1f982d5f9ba3ef160926f9c538ba
Dilip Kumar <dilipkumar@Dilips-Mac.local>
2023-12-09 01:41:36 +0530

    post section added

--- app/page.jsx
+++ app/page.jsx
@@ -28,7 +28,25 @@ export default function Home() {
             <a href="#">View all</a>
           </div>
           <div className="posts">
+            <a className="post">
+              <h2>Making a design system from scratch</h2>
+              <div className="archive">
+                <p className="date">12 Feb 2020</p>
+                <div className="seperator"></div>
+                <p className="tags">Design, Pattern</p>
+              </div>
+              <p className="excerpt">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
+            </a>
+            <a className="post" href="#">
+              <h2>Making a design system from scratch</h2>
+              <div className="archive">
+                <p className="date">12 Feb 2020</p>
+                <div className="seperator"></div>
 
+                <p className="tags">Design, Pattern</p>
+              </div>
+              <p className="excerpt">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
+            </a>
           </div>
         </div>
       </section>
