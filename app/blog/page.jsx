d9ce2bbb97657db25609c25e4543ffac61bde14b
Dilip Kumar <dilipkumar@Dilips-Mac.local>
2023-12-09 22:02:44 +0530

    blog archive added

---
+++ app/blog/page.jsx
@@ -0,0 +1,78 @@
+import Footer from "../components/Footer"
+import "./blog.css"
+export default function Blog() {
+    return (
+
+        <>
+            <section id="blog-page">
+                <div className="container">
+                    <div className="heading">
+                        <h1>Blog</h1>
+                    </div>
+                    <div className="blog">
+                        <h2>Making a design system from scratch</h2>
+                        <div className="archive">
+                            <p className="date">12 Feb 2020</p>
+                            <div className="seperator"></div>
+                            <p className="tags">Design, Pattern</p>
+                        </div>
+                        <p className="excerpt">
+                            Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+                            amet sint. Velit officia consequat duis enim velit mollit.
+                            Exercitation veniam consequat sunt nostrud amet.
+                        </p>
+                    </div> <div className="blog">
+                        <h2>Making a design system from scratch</h2>
+                        <div className="archive">
+                            <p className="date">12 Feb 2020</p>
+                            <div className="seperator"></div>
+                            <p className="tags">Design, Pattern</p>
+                        </div>
+                        <p className="excerpt">
+                            Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+                            amet sint. Velit officia consequat duis enim velit mollit.
+                            Exercitation veniam consequat sunt nostrud amet.
+                        </p>
+                    </div> <div className="blog">
+                        <h2>Making a design system from scratch</h2>
+                        <div className="archive">
+                            <p className="date">12 Feb 2020</p>
+                            <div className="seperator"></div>
+                            <p className="tags">Design, Pattern</p>
+                        </div>
+                        <p className="excerpt">
+                            Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+                            amet sint. Velit officia consequat duis enim velit mollit.
+                            Exercitation veniam consequat sunt nostrud amet.
+                        </p>
+                    </div> <div className="blog">
+                        <h2>Making a design system from scratch</h2>
+                        <div className="archive">
+                            <p className="date">12 Feb 2020</p>
+                            <div className="seperator"></div>
+                            <p className="tags">Design, Pattern</p>
+                        </div>
+                        <p className="excerpt">
+                            Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+                            amet sint. Velit officia consequat duis enim velit mollit.
+                            Exercitation veniam consequat sunt nostrud amet.
+                        </p>
+                    </div> <div className="blog">
+                        <h2>Making a design system from scratch</h2>
+                        <div className="archive">
+                            <p className="date">12 Feb 2020</p>
+                            <div className="seperator"></div>
+                            <p className="tags">Design, Pattern</p>
+                        </div>
+                        <p className="excerpt">
+                            Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+                            amet sint. Velit officia consequat duis enim velit mollit.
+                            Exercitation veniam consequat sunt nostrud amet.
+                        </p>
+                    </div>
+                </div>
+            </section>
+            <Footer />
+        </>
+    )
+} 
