9f48121e8a9c439781ca87b62f4f700889c50716
Dilip Kumar Maurya <curiousdilip@gmail.com>
2023-12-11 10:06:26 +0000

    works page added

--- app/works/page.jsx
+++ app/works/page.jsx
@@ -1,3 +1,129 @@
+import Image from "next/image";
+import Footer from "../components/Footer";
+import "./works.css";
 export default function works() {
-  return <></>;
+  return (
+    <>
+      <section id="works-page">
+        <div className="container">
+          <div className="heading">
+            <h1>Work</h1>
+          </div>
+          <div className="work-list">
+            <div className="work">
+              <div className="image">
+                <Image
+                  src="/subhash-jha.jpeg"
+                  width={246}
+                  height={180}
+                  alt="Dashboard"
+                />
+              </div>
+              <div className="description">
+                <h3>Designing Dashboards</h3>
+                <div className="meta">
+                  <p className="year">2020</p>
+                  <p className="type">Dashboard</p>
+                </div>
+                <p>
+                  Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+                  amet sint. Velit officia consequat duis enim velit mollit.
+                  Exercitation veniam consequat sunt nostrud amet.
+                </p>
+              </div>
+            </div>
+            <div className="work">
+              <div className="image">
+                <Image
+                  src="/subhash-jha.jpeg"
+                  width={246}
+                  height={180}
+                  alt="Dashboard"
+                />
+              </div>
+              <div className="description">
+                <h3>Designing Dashboards</h3>
+                <div className="meta">
+                  <p className="year">2020</p>
+                  <p className="type">Dashboard</p>
+                </div>
+                <p>
+                  Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+                  amet sint. Velit officia consequat duis enim velit mollit.
+                  Exercitation veniam consequat sunt nostrud amet.
+                </p>
+              </div>
+            </div>
+            <div className="work">
+              <div className="image">
+                <Image
+                  src="/subhash-jha.jpeg"
+                  width={246}
+                  height={180}
+                  alt="Dashboard"
+                />
+              </div>
+              <div className="description">
+                <h3>Designing Dashboards</h3>
+                <div className="meta">
+                  <p className="year">2020</p>
+                  <p className="type">Dashboard</p>
+                </div>
+                <p>
+                  Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+                  amet sint. Velit officia consequat duis enim velit mollit.
+                  Exercitation veniam consequat sunt nostrud amet.
+                </p>
+              </div>
+            </div>
+            <div className="work">
+              <div className="image">
+                <Image
+                  src="/subhash-jha.jpeg"
+                  width={246}
+                  height={180}
+                  alt="Dashboard"
+                />
+              </div>
+              <div className="description">
+                <h3>Designing Dashboards</h3>
+                <div className="meta">
+                  <p className="year">2020</p>
+                  <p className="type">Dashboard</p>
+                </div>
+                <p>
+                  Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+                  amet sint. Velit officia consequat duis enim velit mollit.
+                  Exercitation veniam consequat sunt nostrud amet.
+                </p>
+              </div>
+            </div>
+            <div className="work">
+              <div className="image">
+                <Image
+                  src="/subhash-jha.jpeg"
+                  width={246}
+                  height={180}
+                  alt="Dashboard"
+                />
+              </div>
+              <div className="description">
+                <h3>Designing Dashboards</h3>
+                <div className="meta">
+                  <p className="year">2020</p>
+                  <p className="type">Dashboard</p>
+                </div>
+                <p>
+                  Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+                  amet sint. Velit officia consequat duis enim velit mollit.
+                  Exercitation veniam consequat sunt nostrud amet.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <Footer />
+    </>
+  );
 }
