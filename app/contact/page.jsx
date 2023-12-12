b5f6b04c62d7adbf3aa665977ba8b4d20d05ab4f
Dilip Kumar <dilipkumar@Dilips-Mac.local>
2023-12-12 15:49:47 +0530

    work detail responsive

---
+++ app/contact/page.jsx
@@ -0,0 +1,14 @@
+import "./contact.css"
+export default function Contact() {
+    return (
+        <>
+            <section id="contact">
+                <div className="container">
+                    <div className="heading">
+                        <h1>Contact</h1>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
