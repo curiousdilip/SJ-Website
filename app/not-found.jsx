7f024bdf5b91f0a738cfbe19adb8084b4b106659
Dilip Kumar <dilipkumar@Dilips-Mac.local>
2023-12-14 18:31:47 +0530

    404 page completed

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,13 +1,27 @@
 import Link from "next/link";
-import Footer from "./components/Footer";
+import Image from "next/image";
 
 export default function NotFoundPage() {
   return (
     <>
       <div className="not-found">
-        <Link href="/">Home</Link>
+
+        <h1>Whoops!</h1>
+        <h2>404 Page Not Found</h2>
+        <Image
+          src="/subhash-jha.jpeg"
+          width={800}
+          height={400}
+          alt="Picture of the Subhash Jha"
+        />
+        <h3>Looks like this page went on vacation</h3>
+        <p>Try
+          <Link href="/"> Home </Link>
+          or
+          <Link href="/blog"> Blog </Link>
+          instead
+        </p>
       </div>
-      <Footer />
     </>
   );
 }
